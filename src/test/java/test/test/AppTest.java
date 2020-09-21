d7371822043c987072902f336b4cc73e558cb114
Shobhit Mishra <1645@xceedance.com>
2020-09-21 15:38:07 +0530

    commit

--- src/test/java/test/test/AppTest.java
+++ src/test/java/test/test/AppTest.java
@@ -39,3 +39,16 @@
 //	}
 //	
 //}
+package test.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
